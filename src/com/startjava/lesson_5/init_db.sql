4221eaba8d14895048ed130e18f9428149345a8e
Kvitto <kian04@mail.ru>
2024-07-09 15:32:45 +0300

    Refactoring hw

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -2,18 +2,19 @@
 --echo-all
 \c robots
   
-   DROP TABLE IF EXISTS jaegers;
+DROP TABLE IF EXISTS jaegers;
   
- CREATE TABLE jaegers (
-     id         SERIAL PRIMARY KEY,
-     model_name VARCHAR(30) NOT NULL,
-     mark       VARCHAR(30) NOT NULL,
-     height     INTEGER,
-     weight     INTEGER,
-     status     VARCHAR(11),
-     origin	    VARCHAR(30),
-     launch	    DATE,
-     kaiju_kill INTEGER);
+CREATE TABLE jaegers (
+    id         SERIAL PRIMARY KEY,
+    model_name VARCHAR(30) NOT NULL,
+    mark       VARCHAR(30) NOT NULL,
+    height     INTEGER,
+    weight     INTEGER,
+    status     VARCHAR(11),
+    origin     VARCHAR(30),
+    launch     DATE,
+    kaiju_kill INTEGER
+);
 
 \i 'populate.sql'
 \i 'queries.sql'
